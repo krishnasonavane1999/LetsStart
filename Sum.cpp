71979ff2336f51bf192f13b3fdd53538ce9b649d
Krishna Sukhadev Sonavane <krishnasonavane8@gmail.com>
2021-06-13 11:43:17 +0530

    Initial changes in the sum.cpp file

---
+++ Sum.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int num1, num2){
+
+	in sum = (num1 + num2);
+
+	return sum; 	
+}
+
+int main(){
+	
+	int num1 = 10;		
+	int num2 = 20;		
+
+
+	cout<<sum(num1, num2);
+
+	return 0;
+}
