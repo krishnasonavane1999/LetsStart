5769024187eaf11fb7b707c2999f58ddd40d2d8e
Krishna Sonavane <krishnasonavane8@gmail.com>
2021-06-13 12:06:16 +0530

    Added diff.cpp

---
+++ difference.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int diff(int num1, int num2){
+
+	int div = (num1 - num2);
+
+	return div; 	
+}
+
+int main(){
+	
+	int num1 = 10;		
+	int num2 = 20;		
+
+
+	cout<<"Difference of the two numbers is: "<<(num1, num2);
+
+	return 0;
+}
