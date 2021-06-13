1c45b440aa3f8aa17afc9ff369b9f3e7e24a74a4
Krishna Sukhadev Sonavane <krishnasonavane8@gmail.com>
2021-06-13 11:50:30 +0530

    Added new faeture of division of two numbers

---
+++ Division.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+float division(float num1, float num2){
+
+	float div = (num1 + num2);
+
+	return div; 	
+}
+
+int main(){
+	
+	float num1 = 10;		
+	float num2 = 20;		
+
+
+	cout<<"Division of the two numbers is: "<<(num1, num2);
+
+	return 0;
+}
